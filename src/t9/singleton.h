2c4c5131627c653402cfeaec1d1b6f3644017627
te9yie <te9yie@users.noreply.github.com>
2022-10-15 00:48:18 +0900

    Add t9

---
+++ src/t9/singleton.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <cassert>
+
+#include "noncopyable.h"
+
+namespace t9 {
+
+// prevent unintended ADL.
+namespace singleton_ {
+
+// Singleton.
+template <typename T>
+class Singleton : NonCopyable {
+ private:
+  static inline T* instance_ = nullptr;
+
+ protected:
+  Singleton() {
+    assert(!instance_);
+    instance_ = static_cast<T*>(this);
+  }
+  ~Singleton() { instance_ = nullptr; }
+
+ public:
+  static T* instance() { return instance_; }
+};
+
+}  // namespace singleton_
+
+template <typename T>
+using Singleton = singleton_::Singleton<T>;
+
+}  // namespace t9
