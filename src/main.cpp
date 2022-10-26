377b3f33efc9c30a8a4e123f84a1c6cd9aa7a239
te9yie <te9yie@users.noreply.github.com>
2022-10-26 18:02:19 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <SDL.h>
 
 #include <algorithm>
+#include <cstdlib>
 
 #include "imgui.h"
 #include "sai/core/frame.h"
@@ -18,6 +19,13 @@
 
 namespace {
 
+template <typename T>
+T rand_i(int min, int max) {
+  return static_cast<T>(std::rand() % (max - min) + min);
+}
+
+struct CreateRect {};
+
 struct MovementComponent {
   int x;
   int y;
@@ -26,29 +34,38 @@ struct MovementComponent {
 };
 
 struct SpriteComponent {
-  sai::graphics::SpriteHandle sprite_handle;
+  sai::graphics::SpriteHandle handle;
 };
 
-bool create_ball_entity(sai::ecs::Registry* registry,
-                        sai::graphics::SpriteStorage* sprites) {
-  auto id = registry->create_entity<MovementComponent, SpriteComponent>();
-
-  if (auto mc = registry->get<MovementComponent>(id)) {
-    mc->x = 0;
-    mc->y = 0;
-    mc->vx = 3;
-    mc->vy = 3;
+bool setup_rects(sai::task::EventWriter<CreateRect> writer) {
+  for (int i = 0; i < 10; ++i) {
+    writer.notify(CreateRect{});
   }
-
-  if (auto sc = registry->get<SpriteComponent>(id)) {
-    SDL_Rect rect{0, 0, 10, 10};
-    sai::graphics::RGBA color{0xff, 0xff, 0x00, 0xff};
-    auto mat = std::make_unique<sai::graphics::ColorMaterial>(color);
-    sc->sprite_handle =
-        sprites->create(sai::graphics::Sprite{rect, std::move(mat)});
-  }
-
   return true;
+}
+
+void create_rects(sai::ecs::Registry* registry,
+                  sai::graphics::SpriteStorage* sprites,
+                  const sai::video::RenderSize* size,
+                  sai::task::EventReader<CreateRect> reader) {
+  reader.each([&](auto) {
+    auto id = registry->create_entity<MovementComponent, SpriteComponent>();
+
+    if (auto mc = registry->get<MovementComponent>(id)) {
+      mc->x = rand_i<int>(0, size->w);
+      mc->y = rand_i<int>(0, size->h);
+      mc->vx = rand_i<int>(1, 3) * (rand_i<int>(0, 1) ? 1 : -1);
+      mc->vy = rand_i<int>(1, 3) * (rand_i<int>(0, 1) ? 1 : -1);
+    }
+
+    if (auto sc = registry->get<SpriteComponent>(id)) {
+      SDL_Rect rect{0, 0, 10, 10};
+      sai::graphics::RGBA color{rand_i<Uint8>(0, 0xff), rand_i<Uint8>(0, 0xff),
+                                rand_i<Uint8>(0, 0xff), 0xff};
+      auto mat = std::make_unique<sai::graphics::ColorMaterial>(color);
+      sc->handle = sprites->create(sai::graphics::Sprite{rect, std::move(mat)});
+    }
+  });
 }
 
 void update_movement(sai::ecs::Query<MovementComponent&> query,
@@ -72,7 +89,7 @@ void update_sprites(
     sai::ecs::Query<const MovementComponent&, SpriteComponent&> query,
     sai::graphics::SpriteStorage* sprites) {
   for (auto [mc, sc] : query) {
-    auto sprite = sprites->get(sc.sprite_handle);
+    auto sprite = sprites->get(sc.handle);
     sprite->rect.x = mc.x - 5;
     sprite->rect.y = mc.y - 5;
   }
@@ -80,8 +97,25 @@ void update_sprites(
 
 void render_debug_gui(sai::debug::Gui*, const sai::core::Frame* frame,
                       sai::task::Scheduler* scheduler,
-                      sai::debug::PerformanceProfiler* profiler) {
+                      sai::debug::PerformanceProfiler* profiler,
+                      sai::task::EventWriter<CreateRect> writer) {
   ImGui::Begin("Debug");
+  if (ImGui::Button("Create")) {
+    writer.notify(CreateRect{});
+  }
+  ImGui::SameLine();
+  if (ImGui::Button("Create 10")) {
+    for (int i = 0; i < 10; ++i) {
+      writer.notify(CreateRect{});
+    }
+  }
+  ImGui::SameLine();
+  if (ImGui::Button("Create 100")) {
+    for (int i = 0; i < 100; ++i) {
+      writer.notify(CreateRect{});
+    }
+  }
+  ImGui::Separator();
   sai::core::render_debug_gui(frame);
   if (ImGui::CollapsingHeader("Tasks")) {
     ImGui::Indent();
@@ -102,9 +136,11 @@ int main(int /*argc*/, char* /*argv*/[]) {
   sai::task::App app;
 
   app.add_context<sai::ecs::Registry>();
+  app.add_event<CreateRect>();
 
-  app.add_setup_task(create_ball_entity);
+  app.add_setup_task(setup_rects);
 
+  app.add_task("create rects", create_rects);
   app.add_task("update movecomponents", update_movement);
   app.add_task("apply sprites", update_sprites);
   app.add_task("render debug gui", render_debug_gui);
